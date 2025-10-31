a8ffd7cf40f0f3d3710a4a8de502891ae7df76bc
Kohxax <koharen0256@gmail.com>
2025-10-31 20:21:41 +0900

    ドロップアイテム用データクラス追加その3

--- src/main/kotlin/dev/bokukoha/rareDropExtends/data/ArtifactPart.kt
+++ src/main/kotlin/dev/bokukoha/rareDropExtends/data/ArtifactPart.kt
@@ -1,16 +1,13 @@
 package dev.bokukoha.rareDropExtends.data
 
 import org.bukkit.Material
-import org.bukkit.enchantments.Enchantment
 
 /*
  * ドロップするカスタムパーツのデータ
  */
 data class ArtifactPart(
-    val key: String,
-    val type: ArtifactType,
+    val id: String,
+    val name: String,
     val material: Material,
-    val displayName: String,
-    val enchantments: Map<Enchantment, Int> = emptyMap(),
-    val lore: List<String> = emptyList()
+    val type: ArtifactType
 )
