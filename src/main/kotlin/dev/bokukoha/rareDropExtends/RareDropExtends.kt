7c3bed67f20dec52eb13f984cd7f570d52e425e8
Kohxax <koharen0256@gmail.com>
2025-10-31 20:22:07 +0900

    コンフィグ生成とロードのロジック追加

--- src/main/kotlin/dev/bokukoha/rareDropExtends/RareDropExtends.kt
+++ src/main/kotlin/dev/bokukoha/rareDropExtends/RareDropExtends.kt
@@ -4,8 +4,13 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class RareDropExtends : JavaPlugin() {
 
+    private lateinit var manager: ArtifactManager
+
     override fun onEnable() {
         // Plugin startup logic
+        saveDefaultConfig()
+        manager = ArtifactManager(this)
+        manager.loadConfigValues()
     }
 
     override fun onDisable() {
