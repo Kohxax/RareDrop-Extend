bb3049012c2b7d2bedb33ec788c55b00a9e80927
Kohxax <koharen0256@gmail.com>
2025-11-01 15:45:35 +0900

    メインクラス修正

--- src/main/kotlin/dev/bokukoha/rareDropExtends/RareDropExtends.kt
+++ src/main/kotlin/dev/bokukoha/rareDropExtends/RareDropExtends.kt
@@ -1,19 +1,34 @@
 package dev.bokukoha.rareDropExtends
 
+import dev.bokukoha.rareDropExtends.listener.CombatListener
+import dev.bokukoha.rareDropExtends.listener.FishingListener
+import dev.bokukoha.rareDropExtends.listener.MiningListener
+import dev.bokukoha.rareDropExtends.recipes.ArtifactCrafting
 import org.bukkit.plugin.java.JavaPlugin
 
-class RareDropExtends : JavaPlugin() {
+class ArtifactPlugin : JavaPlugin() {
 
     private lateinit var manager: ArtifactManager
 
     override fun onEnable() {
-        // Plugin startup logic
+        logger.info("§a[Artifacts] プラグインを読み込み中...")
         saveDefaultConfig()
+
+        // 設定読み込み
         manager = ArtifactManager(this)
         manager.loadConfigValues()
+
+        // イベント登録
+        server.pluginManager.registerEvents(FishingListener(this), this)
+        server.pluginManager.registerEvents(MiningListener(this), this)
+        server.pluginManager.registerEvents(CombatListener(this), this)
+        server.pluginManager.registerEvents(ArtifactCrafting(this), this)
+
+        logger.info("§a[Artifacts] 初期化完了！")
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
+        logger.info("§c[Artifacts] プラグインを停止しました。")
     }
 }
+
