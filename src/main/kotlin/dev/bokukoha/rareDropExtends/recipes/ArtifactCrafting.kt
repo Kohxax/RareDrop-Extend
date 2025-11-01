6982168e853ef0cc0d054037c2cc544104f204d2
Kohxax <koharen0256@gmail.com>
2025-11-01 16:45:28 +0900

    完成品に作成者名入れるようにした

--- src/main/kotlin/dev/bokukoha/rareDropExtends/recipes/ArtifactCrafting.kt
+++ src/main/kotlin/dev/bokukoha/rareDropExtends/recipes/ArtifactCrafting.kt
@@ -2,17 +2,20 @@ package dev.bokukoha.rareDropExtends.recipes
 
 import dev.bokukoha.rareDropExtends.data.ArtifactType
 import dev.bokukoha.rareDropExtends.util.ItemFactory
+import org.bukkit.NamespacedKey
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.PrepareItemCraftEvent
 import org.bukkit.persistence.PersistentDataType
 import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.NamespacedKey
+import java.time.LocalDate
 
 class ArtifactCrafting(private val plugin: JavaPlugin) : Listener {
 
     @EventHandler
     fun onCraft(e: PrepareItemCraftEvent) {
+        val player = e.view.player as? Player ?: return
         val items = e.inventory.matrix.filterNotNull()
         if (items.size < 3) return
 
@@ -25,7 +28,21 @@ class ArtifactCrafting(private val plugin: JavaPlugin) : Listener {
 
         for (type in ArtifactType.values()) {
             if (foundIds.containsAll(type.parts)) {
-                e.inventory.result = ItemFactory.createResult(plugin, type)
+                val result = ItemFactory.createResult(plugin, type)
+
+                val meta = result.itemMeta!!
+                val lore = meta.lore?.toMutableList() ?: mutableListOf()
+                lore.add("§4Crafted by ${player.name}")
+                meta.lore = lore
+
+                meta.persistentDataContainer.set(
+                    NamespacedKey(plugin, "crafted_by"),
+                    PersistentDataType.STRING,
+                    player.name
+                )
+
+                result.itemMeta = meta
+                e.inventory.result = result
                 return
             }
         }
@@ -33,3 +50,4 @@ class ArtifactCrafting(private val plugin: JavaPlugin) : Listener {
         e.inventory.result = null
     }
 }
+
